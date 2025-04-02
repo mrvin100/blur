2fe616eba038ab50e89c4129d06a4f86762cd458
TIOKOU Jake Melvin <138882910+jakemelvin@users.noreply.github.com>
2025-04-02 19:27:38 +0100

    fix: arrange the hook problem

--- hooks/useRaceParty.ts
+++ hooks/useRaceParty.ts
@@ -1,11 +1,28 @@
-import { useQuery } from "@tanstack/react-query"
-import { RaceCacheKeys } from "./const"
-import { getRaceByPartyId } from "@/app/api/race/route"
+import { useMutation, useQuery } from "@tanstack/react-query"
+import { PartyManagementCacheKeys, RaceCacheKeys } from "./const"
+import { addRace, getRaceByPartyId } from "@/app/api/race/route"
+import { queryClient } from "@/app/dashboard/layout"
 
 export const useRaceParty = (partyId: string) => {
   const fetchRaceByPartyId = useQuery({
     queryKey: [RaceCacheKeys.Race],
     queryFn: () => getRaceByPartyId(partyId)
   })
-  return { fetchRaceByPartyId }
+
+  const createRace = useMutation({
+    mutationFn: () => addRace(partyId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [RaceCacheKeys.Race]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [PartyManagementCacheKeys.Party]
+      })
+      queryClient.invalidateQueries({
+        queryKey: [PartyManagementCacheKeys.Parties]
+      })
+    }
+
+  })
+  return { fetchRaceByPartyId, createRace }
 }
