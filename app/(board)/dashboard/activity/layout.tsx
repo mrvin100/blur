e491a50e53d7a064ee988f96a3eda6ceb595bce3
mrvin100 <vincentyoumssi@example.com>
2025-04-06 23:19:58 +0100

    update : board items

--- app/(board)/dashboard/activity/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DefaultRouteLayout } from "@/modules/shared/components";
-
-export default function ActivityLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DefaultRouteLayout>{children}</DefaultRouteLayout>;
-} 
